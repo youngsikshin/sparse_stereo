7c4f722fc87bfa4ed343328b5a57971649ec0d0a
youngsikshin <bluevow@gmail.com>
2017-10-20 18:14:56 +0900

    first commit

---
+++ src/sparse_stereo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    string data_dir("/var/data/kitti/dataset/sequences/00/");
+    string left_dir("image_0/");
+    string right_dir("image_1/");
+
+    cerr << "Hello Stereo!" << endl;
+    return 0;
+}
